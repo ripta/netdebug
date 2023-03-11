63be1a65846e1c2f4e19add1b2573797b090b397
Ripta Pasay <ripta@users.noreply.github.com>
2023-03-11 18:12:05 +0700

    Revert "pkg/echo: implement auto server mode that enables gRPC if a certificate was provided"
    This reverts commit 1f54ee4ff562a16b545f4ad40c291a9999914d35.

--- pkg/echo/echo.go
+++ pkg/echo/echo.go
@@ -40,15 +40,13 @@ type Server struct {
 type ServerMode enumflag.Flag
 
 const (
-	ServerModeAuto ServerMode = iota
-	ServerModeHTTP
+	ServerModeHTTP ServerMode = iota
 	ServerModeGRPC
 	ServerModeBoth
 )
 
 var ServerModeOptions = map[ServerMode][]string{
-	ServerModeAuto: {"", "auto"},
-	ServerModeHTTP: {"http"},
+	ServerModeHTTP: {"", "http"},
 	ServerModeGRPC: {"grpc"},
 	ServerModeBoth: {"grpc+http", "both"},
 }
@@ -72,7 +70,7 @@ func New() *Server {
 	return &Server{
 		Hostname:     hostname,
 		ListenPort:   8080,
-		Mode:         ServerModeAuto,
+		Mode:         ServerModeHTTP,
 		PodName:      getEnvOrDefault("POD_NAME", "($POD_NAME unset)"),
 		PodNamespace: string(ns),
 		PodNode:      getEnvOrDefault("NODE_NAME", "($NODE_NAME unset)"),
@@ -87,7 +85,7 @@ func (s *Server) Run(ctx context.Context) error {
 		return errors.New("--tls-autogenerate cannot be combined with --tls-key-file and --tls-cert-file")
 	} else if (s.TLSCertPath != "") != (s.TLSKeyPath != "") {
 		return errors.New("--tls-key-file and --tls-cert-file must both be empty or both be specified")
-	} else if (s.Mode == ServerModeBoth || s.Mode == ServerModeGRPC) && s.TLSKeyPath == "" && !s.TLSAutogen {
+	} else if s.Mode != ServerModeHTTP && s.TLSKeyPath == "" && !s.TLSAutogen {
 		return errors.New("--mode=grpc currently requires TLS to be enabled (--tls-{cert,key}-file or --tls-autogenerate)")
 	}
 
@@ -137,14 +135,6 @@ func (s *Server) Run(ctx context.Context) error {
 
 		s.TLSCertPath = servCertFile
 		s.TLSKeyPath = servKeyFile
-	}
-
-	if s.Mode == ServerModeAuto {
-		if s.TLSCertPath != "" && s.TLSKeyPath != "" {
-			s.Mode = ServerModeBoth
-		} else {
-			s.Mode = ServerModeHTTP
-		}
 	}
 
 	mux := http.NewServeMux()
